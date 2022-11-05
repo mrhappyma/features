7b88dec079a33d955b8994709d3bacf656cbb900
Dominic Ruggiero <dominic@userexe.me>
2022-11-05 23:54:15 +0000

    there must be a better way to test this lol

--- src/thefuck/install.sh
+++ src/thefuck/install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 sudo apt update
-sudo apt install python3-dev python3-pip python3-setuptools
+sudo apt install python3-dev python3-pip python3-setuptools -y
 pip3 install thefuck --user
 
 echo "eval $(thefuck --alias)" > ~/.bashrc
